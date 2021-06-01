9258ff2f9d7b1f842e1ae603ddf94bac5573b4e0
hmujtaba1101 <47015339+hmujtaba1101@users.noreply.github.com>
2021-06-01 22:04:10 +0500

    Add files via upload

---
+++ src/constants/Image.js
@@ -0,0 +1,12 @@
+const IMAGE = {
+    ICONE_MENU:require("../image/download.jpg"),
+    ICON_USER_DEFAULT:require('../image/avatardefault_92824.png'),
+    ICON_HOME:require('../image/home--v2.png'),
+    ICON_SETTING:require('../image/free-settings-icon-778-thumb.png'),
+    ICON_ABOUT:require('../image/82-827125_round-about-us-icons-hd-png-download.png'),
+    ICON_BLIND:require('../image/blindbb.gif'),
+    ICON_WHITE_HOME:require('../image/home-page.png'),
+    ICON_WHITE_SETTING:require('../image/download2.png'),
+    ICON_WHITE_ABOUT:require('../image/a74003ea3475842f873039eac6e0d7ee179bf76672c09ff6c1be76619b6ffb3c.jpg')
+}
+export {IMAGE}
